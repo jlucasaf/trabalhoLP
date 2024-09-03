5193b14a718666a245d23a48c595b10da7d500a9
Guilherme <gui.liraribeiro10@gmail.com>
2024-09-03 18:36:31 -0300

    falhando no teste 2 de dar resposta valida ao cliente para criacao de usuario

--- server/src/tests/userRoutes.test.ts
+++ server/src/tests/userRoutes.test.ts
@@ -12,4 +12,21 @@ describe('Rotas de usuario funcionam corretamente', () => {
     expect(response.status).toBe(400);
     expect(response.body).toHaveProperty('message', 'Dados de usuário inválidos');
   });
+
+  test('Criaçaõ de usuário com dados certos gera resposta de sucesso', async () => {
+    const validUser = {
+      username:'Username',
+      email: 'user@gmail.com',
+      password: 'senha123',
+      birthDate: '2000-01-01'
+    }
+
+    const response: Response = await request(app)
+                                      .post('/api/register')
+                                      .send(validUser)
+                                      .set('Accept', 'application/json');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('message', 'Usuário criado com sucesso');
+  });
 }) 
