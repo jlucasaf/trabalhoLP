36f9f9adf93a23ac15a91bb0af24e76335110d02
Guilherme <gui.liraribeiro10@gmail.com>
2024-09-03 17:50:00 -0300

    teste de validacao de dados recebidos para criacao de usuario

--- server/src/tests/userRoutes.test.ts
+++ server/src/tests/userRoutes.test.ts
@@ -1,19 +1,15 @@
-import request from "supertest";
+import request, { Response } from "supertest";
 import app from "../app";
 
 describe('Rotas de usuario funcionam corretamente', () => {
   // Teste de exemplo
-  test('Criação de novo usuário tem resposta esperada', async () => {
-
-    const newUser = {username:'Nome de Usuário',
-                    email:'email@email.com',
-                    password:'senha3454321'};
-
-    const response = await request(app)
-                        .post('/api/register')
-                        .send(newUser)
-                        .set('Accept', 'application/json');
+  test('Criação de novo usuário com dados inválidos tem resposta errada', async () => {
+    const response: Response = await request(app)
+                                      .post('/api/register')
+                                      .send({})
+                                      .set('Accept', 'application/json');
 
     expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('message', 'Dados de usuário inválido');
   });
 }) 
