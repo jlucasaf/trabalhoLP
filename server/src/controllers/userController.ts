a81227d644178f6ae09b91cf8f661dcf1392da1f
Guilherme <gui.liraribeiro10@gmail.com>
2024-09-04 13:46:49 -0300

    passando em (email ja cadastrado resulta em mensagem adequada), falhando em (login rejeita usuario nao cadastrado)

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -6,12 +6,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// const DUPLICATE_KEY_ERR = 11000; // Código de erro do mongoDb
+const DUPLICATE_KEY_ERR = 11000; // Código de erro do mongoDb
 const access_token_secret = process.env.ACCESS_TOKEN_SECRET || "default";
 
 export async function register(req: Request, res: Response) {
   const userData = req.body;
-  
+ 
+  // Valida o usuario usando o joi
   const {error} = userValidation.validate(userData);
 
   if (error) {
@@ -24,14 +25,31 @@ export async function register(req: Request, res: Response) {
 
   const newUser = new User(userData)
   
-  const savedUser = await newUser.save();
+  // Salva usuário se nenhum erro ocorre
+  try {
+    const savedUser = await newUser.save();
+    
+    const newUserToken = sign({id:savedUser.id, 
+                              email:userData.email}, 
+                              access_token_secret);
+    
+    return res.status(200).json({
+      success:true,
+      message:'Usuário criado com sucesso.', 
+      token:newUserToken,
+      user: {
+        id: savedUser.id,
+        nome: savedUser.nome,
+        email: savedUser.email,
+      }});
 
-  const newUserToken = sign({id:savedUser.id, email:userData.email}, access_token_secret);
-  
-  return res.status(200).json({
-    success:true,
-    message:'Usuário criado com sucesso.', 
-    token:newUserToken});
+  } catch (error:any) {
+    if (error.code === DUPLICATE_KEY_ERR) return res.status(400).json({
+      success:false,
+      message:'Endereço de email já cadastrado.'
+    });
+    return res.status(400).json({success:false, message:'Um erro inesperado aconteceu'});
+  } 
 }
 
 export async function login(_req: Request, res: Response) {
--- server/src/tests/userRoutes.test.ts
+++ server/src/tests/userRoutes.test.ts
@@ -15,42 +15,63 @@ beforeAll(async () => {
 });
 
 afterAll(async () => {
+  // Assegura que operações feitas nesses teste
+  // não interfiram nas execuções posteriores
   await User.deleteMany({});
   await disconnectDB();
 });
 
-describe('Criação de usuário funciona como esperado', () => {
-  // Teste de exemplo
+describe('Criação de usuário funciona como esperado', () => { 
   test('Usuário no formato inválido é rejeitado pelo servidor.', async () => {
+    // Tenta criar um usuário enviando um objeto vazio
     const response: Response = await request(app)
                                       .post('/register')
                                       .send({})
                                       .set('Accept', 'application/json');
 
-    expect(response.status).toBe(400);
+    expect(response.status).toBe(400); // Bad request
     expect(response.body).toHaveProperty('message', 'Dados de usuário inválidos');
   });
 
+
   test('Usuário com formato válido não é rejeitado pelo servidor.', async () => {
+    // Tenta criar um usuário enviando um objeto com propriedades esperadas
     const response: Response = await request(app)
                                       .post('/register')
                                       .send(validUser)
                                       .set('Accept', 'application/json');
 
-    expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty('message', 'Usuário criado com sucesso.');
-    expect(response.body).toHaveProperty('token');
+    expect(response.status).toBe(200); // Sucesso
+    expect(response.body).toHaveProperty('message', 'Usuário criado com sucesso.'); // Mensagem legível
+    expect(response.body).toHaveProperty('token'); // Token para garantir autenticação logo após criação de conta
+
+    // Verificando se o usuário foi salvo corretamente (exceto senha, que está hasheada)
+    const savedUser = await User.findOne({ email: validUser.email });
+    expect(savedUser).toBeTruthy();
+    expect(savedUser?.email).toBe(validUser.email);
+    expect(savedUser?.nome).toBe(validUser.nome);
   });
 
   test('Usuário com email já cadastrado resulta em mensagem de erro esperada.', async () => {
+    // Tentando criar um usuário com o mesmo email do usuário criado no caso de teste anterior
     const response: Response = await request(app)
                                       .post('/register')
                                       .send(validUser)
                                       .set('Accept','application/json');
 
-    expect(response.status).toBe(401);
-    expect(response.body).toHaveProperty('message', 'Email já está registrado.');
+    expect(response.status).toBe(400); // Bad request
+    expect(response.body).toHaveProperty('message', 'Endereço de email já cadastrado.'); // Mensagem explicativa
   });
 });
 
-
+describe('Login de usuário funciona como esperado', () => {
+  test('Login rejeita usuário não cadastrado', async () => {
+    const response: Response = await request(app)
+                                      .post('/login')
+                                      .send({email:'inexistant@gmail.com', password:'senha123'})
+                                      .set('Accept', 'application/json');
+    
+    expect(response.status).toBe(401); // Unauthorized
+    expect(response.body).toHaveProperty('message', 'Endereço de email não cadastrado.');
+  });
+});
