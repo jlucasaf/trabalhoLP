f3c9bc41be694b94925f456e079f9df2e6f74508
Guilherme <gui.liraribeiro10@gmail.com>
2024-09-07 10:39:54 -0300

    passando em (validar doador - objeto sem propriedades requeridas é rejeitado) e falhando em (validar doador - formato invalido é rejeitado)

--- server/src/tests/validacao.test.ts
+++ server/src/tests/validacao.test.ts
@@ -8,7 +8,7 @@ describe('Dados de doador são corretamente validados', () => {
   let next: NextFunction;
   
   beforeEach(() => {
-    req = {params:{}};
+    req = {body:{}};
     res = {
       status: jest.fn().mockReturnThis(),
       json: jest.fn(),
@@ -16,15 +16,58 @@ describe('Dados de doador são corretamente validados', () => {
     next = jest.fn();
   });
 
-
   test('Objeto vazio é rejeitado', () => {
-  
     validaDoador(req as Request, res as Response, next);
 
     expect(res.status).toHaveBeenCalledWith(400);
-    expect(res.json).toHaveProperty('mensagem', 'Dados inválidos');
-    expect(res.json).toHaveProperty('detalhes'); // Lista de erros
+
+    // Acessar o argumento passado para o mock de `res.json`
+    expect(res.json).toHaveBeenCalledWith(expect.objectContaining({
+      sucesso: false,
+      mensagem: 'Dados inválidos',
+      detalhes: expect.arrayContaining([
+        'O nome é obrigatório',
+        'O email é obrigatório',
+        'A senha é obrigatória',
+        'O CPF é obrigatório',
+        'O campo local é obrigatório'
+      ])
+    }));
+
     expect(next).not.toHaveBeenCalled();
   });
 
+  test('Atributos com formato inválido são rejeitados', () => {
+    req.body = {
+      nome: '',
+      email: 'formatoinv',
+      senha: 'a1',
+      CPF: '12345',
+      local: {
+        cidade: '',
+        endereco: 2,
+        CEP: 'aa'
+      }
+    }
+
+    validaDoador(req as Request, res as Response, next);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+
+    // Acessar o argumento passado para o mock de `res.json`
+    expect(res.json).toHaveBeenCalledWith(expect.objectContaining({
+      sucesso: false,
+      mensagem: 'Dados inválidos',
+      detalhes: expect.arrayContaining([
+        'O nome não pode estar vazio',
+        'O email deve estar no formato __@_._',
+        'A senha é obrigatória',
+        'O CPF deve estar no formato 000.000.000-00',
+        'A cidade não pode ser vazia',
+        'O endereço deve ser uma string',
+        'O CEP deve estar no formato 00000-000'
+      ])
+    }));
+  
+  });
 });
--- server/src/utils/validaDoador.ts
+++ server/src/utils/validaDoador.ts
@@ -1,7 +1,54 @@
 import { NextFunction, Request, Response } from 'express';
+import Joi from 'joi';
+
+const esquemaVoluntario = Joi.object({
+  nome: Joi.required().messages({
+    'any.required' : 'O nome é obrigatório'
+  }),
+  email: Joi.required().messages({
+    'any.required' : 'O email é obrigatório'
+  }),
+  senha: Joi.required().messages({
+    'any.required' : 'A senha é obrigatória'
+  }),
+
+  CPF: Joi.required().messages({
+    'any.required' : 'O CPF é obrigatório'
+  }),
+
+  local: Joi.object({
+    cidade: Joi.required().messages({
+      'any.required' : 'A cidade é obrigatória'
+    }),
+
+    endereco: Joi.string().required().messages({
+      'any.required' : 'O endereço é obrigatório'
+    }),
+
+    CEP: Joi.required().messages({
+      'any.required' : 'O CEP é obrigatório'
+    }),
+
+  }).required().messages({
+    'any.required': 'O campo local é obrigatório',
+  }),
+});
 
 const validaDoador = function (req: Request, res: Response, next: NextFunction) {
-  // ...
+  
+  const novoDoador = req.body;
+
+  const {error} = esquemaVoluntario.validate(novoDoador, {abortEarly: false});
+
+  if (error) {
+    const corpoResposta = {
+      sucesso: false,
+      mensagem: 'Dados inválidos',
+      // Lista com as mensagems legíveis de erro:
+      detalhes: error.details.map(detalhe => detalhe.message),
+    }
+    res.status(400).json(corpoResposta);
+  }
 }
 
 export default validaDoador;
