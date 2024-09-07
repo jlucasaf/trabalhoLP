26cbed9e56fa1702257d3c9f046b1aa8bc8d1ae5
Guilherme <gui.liraribeiro10@gmail.com>
2024-09-07 12:15:16 -0300

    passando em (criacao de conta de usuario valido inexistente resulta em sucesso) falhand em (rejeita email ja cadastrado com retorno esperado)

--- server/src/tests/contas.test.ts
+++ server/src/tests/contas.test.ts
@@ -42,9 +42,26 @@ describe('Criação de usuários ocorre como esperado', () => {
                                 .send(doadorValido)
                                 .set('Accept', 'application/json');
     
-    console.log(response.body);
     expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty('mensagem', 'Usuário criado com sucesso');
-    expect(response.body).toHaveProperty('token');
+    expect(response.body).toHaveProperty('mensagem', 'Doador cadastrado com sucesso');
+    expect(response.body).toHaveProperty('dados');
+    expect(response.body.dados).toHaveProperty('token');
+    expect(response.body.dados).toHaveProperty('usuario');
+    expect(response.body.dados.usuario).toHaveProperty('id');
+    expect(response.body.dados.usuario).toHaveProperty('email', doadorValido.email);
   });
+
+ test('Tentar criar um doador com email já cadastrado resulta em fracasso', async () => {
+    const response: Response = await supertest(app)
+                                .post('/api/doador/cadastro')
+                                .send(doadorValido)
+                                .set('Accept', 'application/json');
+    
+    expect(response.status).toBe(200);
+    expect(response.body).toMatchObject({
+      sucesso: false,
+      mensagem: 'Endereço de email já cadastrado',
+    });
+  });
+
 });
