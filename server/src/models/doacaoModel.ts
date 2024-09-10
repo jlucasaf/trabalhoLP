ef2c886011232a6b9374c13736ac8230b1feaa14
Isabela Soares Furlan <isabelasfurlan@gmail.com>
2024-09-09 21:36:17 -0300

    pasta de relatórios

--- server/src/models/doacaoModel.ts
+++ server/src/models/doacaoModel.ts
@@ -2,11 +2,6 @@ import mongoose, {Schema, Document} from 'mongoose';
 
 interface IDoacao extends Document {
     foto: boolean;
-    local: {
-        cidade: string;
-        endereco: string;
-        CEP: string;
-    };
     data: Date;
     id_doador: mongoose.Schema.Types.ObjectId;
     id_voluntario: mongoose.Schema.Types.ObjectId;
@@ -16,11 +11,6 @@ interface IDoacao extends Document {
 
 const DoacaoSchema: Schema = new Schema({
     foto: {type: Boolean, required: true, default: false},
-    local: {
-        cidade: {type: String, required: true},
-        endereco: {type: String, required: true},
-        CEP: {type: String, required: true},
-    },
     data: {type: Date, required: true},
     id_doador: {type: mongoose.Schema.Types.ObjectId, ref: 'Doador', required: true},
     id_voluntario: {type: mongoose.Schema.Types.ObjectId, ref: 'Voluntario', required: true},
